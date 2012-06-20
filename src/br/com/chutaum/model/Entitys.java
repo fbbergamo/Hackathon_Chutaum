5be439d078104e4b97f27236ddeab08ce1e5daf0
fbbergamo <fbbergamo@fbbergamo-AHV>
2012-06-19 22:06:46 -0300

    Change-Id: I6d73ee224388acbc7a7dd7b94b8361d23d273ccb

---
+++ src/br/com/chutaum/model/Entitys.java
@@ -0,0 +1,8 @@
+package br.com.chutaum.model;
+
+public class Entitys {
+	public static String UserAction ="UserAction" ;
+	public static String PoliticianAction ="PoliticianAction" ;
+	public static String PoliticianFollowers ="PoliticianFollowers" ;
+	public static String UserFollowers ="UserFollowers" ;
+}
